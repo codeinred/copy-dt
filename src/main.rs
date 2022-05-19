10ccba4c806b46adad3ebbf5836e36371a36f85f
Alecto Irene Perez <perez.cs@pm.me>
2022-05-19 02:47:09 -0600

    Implement copy-dt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use chrono::offset::Local;
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
+
+type ClipError = Box<dyn std::error::Error>;
+type ClipResult = Result<(), ClipError>;
+
+fn set_clip_contents(str: String) -> ClipResult {
+    let mut clipboard: ClipboardContext = ClipboardProvider::new()?;
+    clipboard.set_contents(str)?;
+    println!("Set new context: {:?}", clipboard.get_contents()?);
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let time = Local::now().format("%Y-%m-%d %H-%M-%S").to_string();
+    
+    if let Err(error) = set_clip_contents(time) {
+        eprintln!("Error: {}", error.as_ref().to_string());
+        std::process::exit(1);
+    }
 }
